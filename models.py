a6215aa19f36872ddb5ce0a09c9656fa5c09ea06
andersparslov <48243763+andersparslov@users.noreply.github.com>
2019-09-27 13:04:00 +0200

    Add files via upload

--- models.py
+++ models.py
@@ -45,7 +45,49 @@ def multi_tilted_loss(quantiles, y, f):
         loss += K.mean(K.maximum(q*e, (q-1)*e))
     return loss
 
-def joint_multilink(num_filters, kernel_length, input_timesteps, num_links, output_timesteps, quantiles, loss):
+## Encoder-decoder convolutional LSTM that estimates quantiles and mean predictions.
+def joint_convLstm(num_filters, kernel_length, input_timesteps, num_links, output_timesteps, quantiles, loss):
+    model = Sequential()
+    model.add(BatchNormalization(name = 'batch_norm_0', input_shape = (input_timesteps, num_links, 1, 1)))
+    model.add(ConvLSTM2D(name ='conv_lstm_1',
+                         filters = num_filters, kernel_size = (kernel_length, 1),                       
+                         padding = 'same', 
+                         return_sequences = True))
+
+    model.add(Dropout(0.21, name = 'dropout_1'))
+    model.add(BatchNormalization(name = 'batch_norm_1'))
+
+    model.add(ConvLSTM2D(name ='conv_lstm_2',
+                         filters = num_filters, kernel_size = (kernel_length, 1), 
+                         padding='same',
+                         return_sequences = False))
+
+    model.add(Dropout(0.20, name = 'dropout_2'))
+    model.add(BatchNormalization(name = 'batch_norm_2'))
+
+    model.add(Flatten())
+    model.add(RepeatVector(output_timesteps))
+    model.add(Reshape((output_timesteps, num_links, 1, num_filters)))
+
+    model.add(ConvLSTM2D(name ='conv_lstm_3',
+                         filters = num_filters, kernel_size = (kernel_length, 1), 
+                         padding='same',
+                         return_sequences = True))
+
+    model.add(Dropout(0.20, name = 'dropout_3'))
+    model.add(BatchNormalization(name = 'batch_norm_3'))
+
+    model.add(ConvLSTM2D(name ='conv_lstm_4',
+                         filters = num_filters, kernel_size = (kernel_length, 1), 
+                         padding='same',
+                         return_sequences = True))
+
+    model.add(TimeDistributed(Dense(units = len(quantiles) + 1, name = 'dense_1')))
+    model.compile(loss = loss, optimizer = 'nadam')
+    return model
+
+## Encoder-decoder LSTM for 
+def convLstm(num_filters, kernel_length, input_timesteps, num_links, output_timesteps, quantiles, loss):
     model = Sequential()
     model.add(BatchNormalization(name = 'batch_norm_0', input_shape = (input_timesteps, num_links, 1, 1)))
     model.add(ConvLSTM2D(name ='conv_lstm_1',
